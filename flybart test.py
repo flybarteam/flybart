86d7d179fe1929be6e3181a7288d3c066edcca31
flybarteam <39278837+flybarteam@users.noreply.github.com>
2018-05-14 22:55:40 +0200

    Add files via upload

--- flybart test.py
+++ flybart test.py
@@ -3,10 +3,10 @@ os.chdir('C:\\Users\\Anders Herseth\\Documents')  # Flytter nåverende arbeidsma
 
 def meldtvind(retninghastighet):
     global vind                                                                 #Gjør vind variabel om til global
-    fil = open('C:\\Users\\Anders Herseth\\Documents\\flybartest.txt', 'r')     #Åpner filen som ligger lagret
+    fil = open('C:\\Users\\Anders Herseth\\Documents\\værmeldinger.txt', 'r')     #Åpner filen som ligger lagret
     lesefil = fil.read()                                                        #Leser filen som ligger lagret
     try: #Legger inn en try og except i tilfelle filen er tom, så krasjer ikke programmet
-        dictionary = json.loads(lesefil)                                            #Gjør om filen til dictionary
+        dictionary = json.loads(lesefil)                                        #Gjør om filen til dictionary
     except json.decoder.JSONDecodeError:
         dictionary = {} #Hvis filen er tom, skal "dictionary" defineres som en tom dictionary
 
@@ -20,12 +20,13 @@ def meldtvind(retninghastighet):
         vind[retninghastighet] += 1
 
     print(vind)
-    #if retninghastighet == 'exit':    # Kan bruke denne hvis vi skal ha dette i en loop
-    fil = open('C:\\Users\\Anders Herseth\\Documents\\flybartest.txt', 'w')   #Åpner filen i skrivemodus
+    fil = open('C:\\Users\\Anders Herseth\\Documents\\værmeldinger.txt', 'w')   #Åpner filen i skrivemodus
     utskrift = json.dumps(vind)                                               #Gjør om filen til en string
     fil.write(utskrift)                                                       #Lagrer filen
     fil.close()                                                               #Lukker filen
     return vind
+
+
 
 print('Skriv inn en ny retning og hastighet i m/s, f.eks slik: sv1')
 retninghastighet = input() #Retning og hastighet
