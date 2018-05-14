bf3b089ee780024264eee0cc1ee635a2cbe318d2
flybarteam <39278837+flybarteam@users.noreply.github.com>
2018-05-14 21:44:21 +0200

    Add files via upload

---
+++ flybart test.py
@@ -0,0 +1,35 @@
+import os, json
+os.chdir('C:\\Users\\Anders Herseth\\Documents')  # Flytter nåverende arbeidsmappe til denne destinasjonen
+
+def meldtvind(retninghastighet):
+    global vind                                                                 #Gjør vind variabel om til global
+    fil = open('C:\\Users\\Anders Herseth\\Documents\\flybartest.txt', 'r')     #Åpner filen som ligger lagret
+    lesefil = fil.read()                                                        #Leser filen som ligger lagret
+    try: #Legger inn en try og except i tilfelle filen er tom, så krasjer ikke programmet
+        dictionary = json.loads(lesefil)                                            #Gjør om filen til dictionary
+    except json.decoder.JSONDecodeError:
+        dictionary = {} #Hvis filen er tom, skal "dictionary" defineres som en tom dictionary
+
+
+    vind = dictionary                           #Kanskje denne ikkje trengs?
+
+    if retninghastighet not in vind.keys():     #Hvis denne retningen og hastighet ikke finnes fra før, lages det ny
+        vind.setdefault(retninghastighet, 0)
+
+    if retninghastighet in vind.keys():         #Hvis denne retningen og hastighet finnes fra før, plusses det på 1
+        vind[retninghastighet] += 1
+
+    print(vind)
+    #if retninghastighet == 'exit':    # Kan bruke denne hvis vi skal ha dette i en loop
+    fil = open('C:\\Users\\Anders Herseth\\Documents\\flybartest.txt', 'w')   #Åpner filen i skrivemodus
+    utskrift = json.dumps(vind)                                               #Gjør om filen til en string
+    fil.write(utskrift)                                                       #Lagrer filen
+    fil.close()                                                               #Lukker filen
+    return vind
+
+print('Skriv inn en ny retning og hastighet i m/s, f.eks slik: sv1')
+retninghastighet = input() #Retning og hastighet
+meldtvind(retninghastighet)
+
+
+
