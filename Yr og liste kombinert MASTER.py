7bb6b395e6f7d7c3b14f53bb4801cd15eb1e1351
flybarteam <39278837+flybarteam@users.noreply.github.com>
2018-05-19 12:29:06 +0200

    Update Yr og liste kombinert MASTER.py

--- Yr og liste kombinert MASTER.py
+++ Yr og liste kombinert MASTER.py
@@ -3,25 +3,29 @@
 
 
 #Import modules
-import urllib.request
-import xml.etree.ElementTree as ET
-import os, json
-import MatiasYr2
-import YrTilListe
-import Datolisteuthenting
-import VindMedDatoListe
 
-MatiasYr2.open_YR()
+import MatiasYr2                #open_YR()      update()         getForecast()
+import YrTilListe               #meldtvind()
+import Datolisteuthenting       #datoListeuthenting()
+import VindMedDatoListe         #datoListe()
+import HenteFlightlog           #sjekkeFlightlog()
+import Flybare_dager
 
+MatiasYr2.open_YR() #Henter værvarselet for dagen imorgen
 
 
 retninghastighet = str(MatiasYr2.getForecast(MatiasYr2.open_YR()))
 
-retninghastighet = retninghastighet.replace('(', '').replace(')', '').replace(",", "").replace("'", "")
+retninghastighet = retninghastighet.replace('(', '').replace(')', '').replace(",", "").replace("'", "") #Fjerner litt drit, kan flyttes til matias sitt program
 
-YrTilListe.meldtvind(retninghastighet)
-VindMedDatoListe.datoListe(retninghastighet)
+#YrTilListe.meldtvind(retninghastighet)#Lager liste med vind som har vært og teller kor mange dager det er
+print('Vind som har vært meldt: \n' + str(YrTilListe.meldtvind(retninghastighet)) + '\n')
 
-Datolisteuthenting.datoListeuthenting()
+VindMedDatoListe.datoListe(retninghastighet)    #Lager liste med vind og dato
+print('Datoer med hvilken retning og styrke den dagen: \n' + str(VindMedDatoListe.datoListe(retninghastighet)) + '\n')
 
+Datolisteuthenting.datoListeuthenting()         #Henter hvilken vind det var for en uke siden
+
+Flybare_dager.flydager()
+print('Dager det var flybart med vind: \n' + str(Flybare_dager.flydager()) + '\n')
 
