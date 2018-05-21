6a3c7d96500d56688828c00b9bbb67c98ce4a18f
flybarteam <39278837+flybarteam@users.noreply.github.com>
2018-05-21 21:44:32 +0200

    Update Yr og liste kombinert MASTER.py

--- Yr og liste kombinert MASTER.py
+++ Yr og liste kombinert MASTER.py
@@ -14,32 +14,33 @@ import Sjanseflybart
 import time
 
 Sjanseflybart.sjanseflybart()
-sistkjørt = open('C:\\Users\\Anders Herseth\\Documents\\sistkjørt.txt', 'r')  # Åpner filen som ligger lagret#
+sistkjørt = open('/home/pi/Flybart/lastrun', 'r')  # Åpner filen som ligger lagret#
 sistkjørt = sistkjørt.read()                                                  # Leser filen som ligger lagret
 
-if str(time.localtime()[2]) != sistkjørt:
+tid = time.localtime()[3]                                                     # Tidspunkt NÅ
 
-    MatiasYr2.open_YR() #Henter værvarselet for dagen imorgen
+if str(time.localtime()[2]) != sistkjørt and tid > 18 and tid < 21:           # Skal kun kjøre mellom kl 18 og 21
 
-
-
-    retninghastighet = str(MatiasYr2.getForecast(MatiasYr2.open_YR()))
-
-    retninghastighet = retninghastighet.replace('(', '').replace(')', '').replace(",", "").replace("'", "") #Fjerner litt drit, kan flyttes til matias sitt program
-
-    #YrTilListe.meldtvind(retninghastighet)#Lager liste med vind som har vært og teller kor mange dager det er
-    print('Vind som har vært meldt: \n' + str(YrTilListe.meldtvind(retninghastighet)) + '\n')
-
-    VindMedDatoListe.datoListe(retninghastighet)    #Lager liste med vind og dato
-    print('Datoer med hvilken retning og styrke den dagen: \n' + str(VindMedDatoListe.datoListe(retninghastighet)) + '\n')
-
-    Datolisteuthenting.datoListeuthenting()         #Henter hvilken vind det var for en uke siden
-
-    Flybare_dager.flydager()
-    print('Dager det var flybart med vind: \n' + str(Flybare_dager.flydager()) + '\n')
+        MatiasYr2.open_YR() #Henter værvarselet for dagen imorgen
 
 
-sistkjørt = open('C:\\Users\\Anders Herseth\\Documents\\sistkjørt.txt', 'w')  # Åpner filen som ligger lagret#
-sistkjørt.write(str(time.localtime()[2]))                                              # Leser filen som ligger lagret
-sistkjørt.close()
+
+        retninghastighet = str(MatiasYr2.getForecast(MatiasYr2.open_YR()))
+
+        retninghastighet = retninghastighet.replace('(', '').replace(')', '').replace(",", "").replace("'", "") #Fjerner litt drit, kan flyttes til matias sitt program
+
+        #YrTilListe.meldtvind(retninghastighet)#Lager liste med vind som har vært og teller kor mange dager det er
+        print('Vind som har vært meldt: \n' + str(YrTilListe.meldtvind(retninghastighet)) + '\n')
+
+        VindMedDatoListe.datoListe(retninghastighet)    #Lager liste med vind og dato
+        print('Datoer med hvilken retning og styrke den dagen: \n' + str(VindMedDatoListe.datoListe(retninghastighet)) + '\n')
+
+        Datolisteuthenting.datoListeuthenting()         #Henter hvilken vind det var for en uke siden
+
+        Flybare_dager.flydager()
+        print('Dager det var flybart med vind: \n' + str(Flybare_dager.flydager()) + '\n')
+
+        sistkjørt = open('/home/pi/Flybart/lastrun', 'w')  # Åpner filen som ligger lagret#
+        sistkjørt.write(str(time.localtime()[2]))                                              # Leser filen som ligger lagret
+        sistkjørt.close()
 
